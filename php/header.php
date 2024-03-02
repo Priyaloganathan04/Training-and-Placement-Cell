ff973de5be10041523e17026d75b77938ffe60c9
Priya Loganathan <126935622+Priyaloganathan04@users.noreply.github.com>
2024-03-02 17:34:54 +0530

    Add files via upload

---
+++ php/header.php
@@ -0,0 +1,53 @@
+ <img src="assets/img/logo1.jpg" height="80px" class="fixed-top" style="height:80px; margin-top:20px; margin-left:10px;" />
+ <br>
+ <header id=" header" class="header fixed-top" data-scrollto-offset="0">
+               <div class="container-fluid d-flex align-items-center justify-content-between">
+
+         <a href="index.php" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
+
+         </a>
+        
+         <nav id="navbar" class="navbar" style="margin-left:5%; margin-top:10px;">
+             <ul>
+
+                 <li><a class="nav-link scrollto" href="index.php">Home</a></li>
+                 <li class="dropdown"><a href="login.php"><span>Login</span> </a>
+                     <ul>
+
+                         <li><a href="admin/index.php">Placement Cell</a></li>
+                         <li><a href="login-candidates.php">Students</a></li>
+                         <li><a href="login-coordinator.php">Co - Ordinators</a></li>
+                         <li><a href="login-company.php">Company</a></li>
+
+
+                     </ul>
+                 </li>
+                 <li class="dropdown"><a href="login.php"><span>Register</span> </a>
+                     <ul>
+
+                         <!-- <li><a href="admin/index.php">Admin</a></li> -->
+                         <li><a href="register-candidates.php">Students</a></li>
+                         <li><a href="register-coordinator.php">Co - Ordinators</a></li>
+                         <li><a href="register-company.php">Company</a></li>
+
+
+                     </ul>
+                 </li>
+                 
+
+                 <!-- <li><a class="nav-link scrollto" href="login.php">Sign Up</a></li> -->
+
+                 <!-- <li><a class="nav-link scrollto" href="faq.php">FAQ</a></li> -->
+
+                 <li><a class="nav-link scrollto" href="contact.php">Contact</a></li>
+                 <!-- <li><a class="nav-link scrollto" href="aboutus.php">FAQ</a></li> -->
+
+
+             </ul>
+             <i class="bi bi-list mobile-nav-toggle d-none"></i>
+         </nav><!-- .navbar -->
+
+         <a class="btn-getstarted scrollto" href="faq.php">FAQ</a>
+
+     </div>
+ </header>

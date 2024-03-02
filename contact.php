d7f63678af759a3427d3e92c89536254bfaffb55
Priya Loganathan <126935622+Priyaloganathan04@users.noreply.github.com>
2024-03-02 17:37:02 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Placement Portal</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <!-- Favicons -->
+    <link href="img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Variables CSS Files. Uncomment your preferred color scheme -->
+    <link href="assets/css/variables.css" rel="stylesheet">
+
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+    <!-- custom css -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="css/AdminLTE.min.css">
+    <link rel="stylesheet" href="css/_all-skins.min.css">
+    <link rel="stylesheet" href="assets/css/chat.css">
+</head>
+
+<body>
+
+    <!-- <header> -->
+
+
+
+    <?php
+
+    include 'php/header.php'
+
+    ?>
+
+
+
+    <!-- </header> -->
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+        <div class="container" style="margin-top: -20px;">
+
+            <div class="section-header text-center">
+                <br>
+                <h2 style="color:#0ea2bd;"><strong>Contact Us</strong></h2>
+            </div>
+
+        </div>
+        <div class="container" style="margin-top: -30px;">
+            <div class="row justify-content-center">
+                <div class="col-lg-4">
+                    <div cleass="info">
+                        <h3 class="text-cnter">Get in touch</h3>
+                        <p class="text-center">PlACEMENT CELL</p>
+
+                        <div class="info-item d-flex">
+                            <i class="bi bi-geo-alt flex-shrink-0"></i>
+                            <div>
+                                <h4>Location:</h4>
+                                <p>Sri Jayendra Saraswathi Street, Enathur, Kanchipuram, Tamil Nadu 631561</p>
+                            </div>
+                        </div><!-- End Info Item -->
+
+                        <div class="info-item d-flex">
+                            <i class="bi bi-envelope flex-shrink-0"></i>
+                            <div>
+                                <h4>Email:</h4>
+                                <p>registrar@kanchiuniv.ac.in</p>
+                            </div>
+                        </div><!-- End Info Item -->
+
+                        <div class="info-item d-flex">
+                            <i class="bi bi-phone flex-shrink-0"></i>
+                            <div>
+                                <h4>Call:</h4>
+                                <p>044 2726 4293</p>
+                            </div>
+                        </div><!-- End Info Item -->
+                    </div>
+                </div>
+                <div class="info col-lg-5 shadow">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2450.384798099715!2d79.72972053904938!3d12.859891054923922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52c20d805eb7bd%3A0x489fbef75f724222!2sSri%20Chandrasekharendra%20Saraswathi%20Viswa%20Mahavidyalaya%20(SCSVMV)!5e0!3m2!1sen!2sin!4v1686385986313!5m2!1sen!2sin" width="400" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div><!-- End Google Maps -->
+            </div>
+
+        </div>
+    </section><!-- End Contact Section -->
+
+
+</body>
+
+</html>
